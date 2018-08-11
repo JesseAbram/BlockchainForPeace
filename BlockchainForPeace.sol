c6343bd46fabb683611e47ec2dd2d1dad390ef63
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2018-08-11 15:33:01 -0400

    added a consturctor

--- BlockchainForPeace.sol
+++ BlockchainForPeace.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
-contract BlockchainForPeace {
+contract BlockchainForPeace{
     
     //address is a place hoder for the address of the charity, this is my address for testing
-    address charity = 0x362f03a4a65162ba13a74b97cd8c921ed38757fb;
-    
+ 
+
     // to see the total raised 
     uint public raised;
-    
+    address public charity; 
     //struct for the donation 
     struct Donation {
         address from; 
@@ -16,6 +16,12 @@ contract BlockchainForPeace {
     }
     
     Donation [128] public board; 
+   
+   
+    constructor (address _charity) public {
+        charity = _charity;
+    }
+   
     
     //function payable takes in address a message and a value, transfers is to set charity address and get a value
     function donate (uint i, bytes32 message) payable public{
@@ -27,9 +33,10 @@ contract BlockchainForPeace {
         });
         
         charity.transfer(msg.value);
+       donate(msg.value, message);
         
         raised += msg.value;
     }
     
-    
+    event Donate (address indexed from, uint amount);
 }
