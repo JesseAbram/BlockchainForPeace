8d8ed2a517811c3462405fe06a71e69c198e2850
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2018-08-13 09:50:51 -0400

    fixed things

--- BlockchainForPeace.sol
+++ BlockchainForPeace.sol
@@ -2,51 +2,34 @@ pragma solidity ^0.4.24;
 
 contract BlockchainForPeace{
     
-    //address is a place hoder for the address of the charity, this is my address for testing
- 
 
     // to see the total raised 
     uint public raised;
     address public charity; 
     //struct for the donation 
+   
     struct Donation {
-        address from; 
-        bytes32 message; 
+        string message; 
         uint value; 
     }
     
-    Donation [128] public board; 
-   
+    mapping (address => Donation) public donors;
+    
    
     constructor (address _charity) public {
         charity = _charity;
     }
    
-    
-    //function payable takes in address a message and a value, transfers is to set charity address and get a value
-    function donate (uint i, bytes32 message) payable public{
-        
-        board [i] = Donation ({
-            from: msg.sender, 
-            message: message, 
-            value: msg.value
-        });
-        
-        charity.transfer(msg.value);
-       donate(msg.value, message);
-        
-        raised += msg.value;
-    }
-    
 
     function () payable public {
-        
         charity.transfer(msg.value);
-        Donate(msg.sender, msg.value);
+        donors[msg.sender].value = msg.value; 
         raised += msg.value;
     }
 
-
+    function messageForPeace(string _message) public {
+        donors[msg.sender].message = _message;
+    }
     
     event Donate (address indexed from, uint amount);
 }
